1a7b4a50ae8277f1a39e4f202726a9d4d3bbc7c9
Daniil <dandonike@gmail.com>
2017-01-08 21:53:47 +0300

    Nothing important

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 setTimeout(function() {
-	$('body').append('<div id="baner"><button id="close">X</button></div>');
+	$('body').append('<div id="baner"><button id="close">X</button><p>Warning</p></div>');
 
 $('#close').click(function() {
 	console.log('0_0');
 	$('#baner').remove();
 });
-}, 2000);
+}, 500);
 
